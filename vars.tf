9bff98a638f3d1f27e3f5d5f23459dd756bea2bb
Ove Ragnar Oddløkken <post@rykroken.net>
2020-02-21 21:59:21 +0100

    Reorder vars.tf

--- vars.tf
+++ vars.tf
@@ -20,46 +20,46 @@ variable tags {
 # EC2
 variable instance_type {
   type        = string
-  default     = "t3.micro"
   description = "The type of instance to start. Updates to this field will trigger a stop/start of the EC2 instance."
+  default     = "t3.micro"
 }
 
 variable instance_name {
   type        = string
-  default     = "wireguard-vpn-node"
   description = "A name to attach to the EC2 instance."
+  default     = "wireguard-vpn-node"
 }
 
 variable mgmt_allowed_hosts {
   type        = list
-  default     = []
   description = "A list of hosts/networks to open up SSH access to."
+  default     = []
 }
 
 variable sg_wg_allowed_subnets {
   type        = list
-  default     = ["0.0.0.0/0"]
   description = "A list of hosts/networks to open up WireGuard access to."
+  default     = ["0.0.0.0/0"]
 }
 
 variable "ssh_public_key" {
   type        = string
-  default     = null
   description = "(Optional) A SSH public key to create a key pair for in AWS EC2."
+  default     = null
 }
 
 
 # DNS
 variable dns_zone {
   type        = string
-  default     = null
   description = "(Optional) The Route53 hosted zone to add DNS records to."
+  default     = null
 }
 
 variable dns_fqdn {
   type        = string
-  default     = null
   description = "(Optional) The FQDN of the A record pointing to the EC2 instance."
+  default     = null
 }
 
 # Wireguard
@@ -69,20 +69,24 @@ variable wg_client_public_keys {
 }
 
 variable "wg_persistent_keepalive" {
-  default     = 25
+  type        = number
   description = "Persistent Keepalive - useful for helping connectiona stability over NATs"
+  default     = 25
 }
 
 variable wg_server_network_cidr {
-  default     = "192.168.2.0/24"
+  type        = string
   description = "The internal network to use for WireGuard. Remember to place the clients in the same subnet."
+  default     = "192.168.2.0/24"
 }
 
 variable "wg_server_port" {
-  default     = 51820
+  type        = number
   description = "The port WireGuard should listen on."
+  default     = 51820
 }
 
 variable wg_privkey_ssm_path {
+  type        = string
   description = "The path to the WireGuard server's private key in SSM"
 }
